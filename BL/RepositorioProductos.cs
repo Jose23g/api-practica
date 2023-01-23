f6e2de36c7e84be0906cd7b7ddd591a1a4ab90f6
José García Borge <adrian23gb@hotmail.com>
2023-01-22 18:41:06 -0600

    cambios

--- BL/RepositorioProductos.cs
+++ BL/RepositorioProductos.cs
@@ -17,6 +17,47 @@ namespace BL
             ElContextoBD= _elContextoBD;
         }
 
+        public Presentacion getIdPrecentacion(string nombre)
+        {
+            try
+            {
+                var presentacion = ElContextoBD.Presentacion
+                                              .Where(x => x.Nombre==nombre)
+                                                .FirstOrDefault();
+                if (presentacion == null)
+                {
+                    Presentacion nuevo = new Presentacion();
+                    nuevo.Nombre = nombre;
+                    ElContextoBD.Presentacion.Add(nuevo);
+                    ElContextoBD.SaveChanges();
+                    return nuevo;
+                }
+                return presentacion;
+            }
+            catch(Exception e)
+            {
+                Presentacion presentacion= new Presentacion();
+                return presentacion;
+            }
+        }
+
+        public Unidad_Medida getIdUnidad_Medida(string nombre)
+        {
+            var unidad = ElContextoBD.Unidad_Medida
+                .Where(x=>x.Nombre == nombre)
+                .FirstOrDefault();
+            if (unidad == null)
+            {
+                Unidad_Medida unidad_Medida= new Unidad_Medida();
+                unidad_Medida.Nombre= nombre;
+                ElContextoBD.Unidad_Medida.Add(unidad_Medida);
+                ElContextoBD.SaveChanges();
+                return unidad_Medida;
+            }
+
+            return unidad;
+        }
+
         public List<Producto> listaProductos()
         {
            return ElContextoBD.Producto.ToList();
