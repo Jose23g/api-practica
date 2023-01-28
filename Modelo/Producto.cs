7d2a87dcf7710f80e30804e4740fa94115f987b7
José García Borge <adrian23gb@hotmail.com>
2023-01-27 21:47:07 -0600

    Se puede agregar un producto con su respectiva presentacion y unidad

--- Modelo/Producto.cs
+++ Modelo/Producto.cs
@@ -15,7 +15,11 @@ namespace Modelo
         public int codigo_producto { get; set; }
         public string nombre { get; set; }
         public float precio_venta { get; set; }
+        /*public int id_presentacion { get; set; }*/
+        [ForeignKey("id_presentacion")]
         public Presentacion presentacion { get; set; }
+       /* public int id_unidad { get; set; }*/
+        [ForeignKey("id_unidad")]
         public Unidad_Medida Unidad_Medida { get; set; }
        
     }
